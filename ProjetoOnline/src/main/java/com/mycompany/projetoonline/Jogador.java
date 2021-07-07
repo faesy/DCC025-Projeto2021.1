dbada3343d26b2f680d5a26672e70fee4a18e87b
matheus <matheusfaesy@gmail.com>
2021-07-07 15:08:25 -0300

    comecando a trabalhar no jogador

--- ProjetoOnline/src/main/java/com/mycompany/projetoonline/Jogador.java
+++ ProjetoOnline/src/main/java/com/mycompany/projetoonline/Jogador.java
@@ -10,4 +10,54 @@ package com.mycompany.projetoonline;
  * @author mathe
  */
 public class Jogador {
+    protected Deus DeusEscolhido;
+    private String nome;
+    private String Senha;
+    private boolean cadastrado;
+    private boolean logado;
+    private Consumivel[] ConsumiveisEquipados=new Consumivel[3];
+     
+     public void MenuDeEscolhas(){
+         
+     }
+     public void MenuDeCombate(){
+         
+     }
+     private void EscolherHablidade(){
+         
+     }
+     private void EscolherConsumivel(){
+         
+     }
+     private void SelecionarDeus(){
+         
+     }
+     public Jogador(){
+        if (this.cadastrado==true){
+                 //Realize o Cadastro
+                 this.Cadastrar();
+         }
+        else{
+            //Realize o Login
+            String nomeDigitado="0";
+            String senhaDigitada="0";
+            if(this.VerificarLogin(nomeDigitado, senhaDigitada)){
+                this.logado=true;
+            }
+        }
+     }
+     
+     private void Deslogar(){
+         this.logado=false;
+     }
+             
+     private void Cadastrar(){
+         
+         
+         
+         this.SelecionarDeus();
+     }
+     private boolean VerificarLogin(String nomeDigitado,String senhaDigitada){
+         
+     }
 }
