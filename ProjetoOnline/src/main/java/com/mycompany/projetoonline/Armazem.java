23bb2ddd440518485f90cf1c0c8f3b43e0f63dc0
thais <thais.soares@estudante.ufjf.br>
2021-07-10 12:09:19 -0300

    cadastrar e logar

--- ProjetoOnline/src/main/java/com/mycompany/projetoonline/Armazem.java
+++ ProjetoOnline/src/main/java/com/mycompany/projetoonline/Armazem.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-*/
+ */
 package com.mycompany.projetoonline;
 
 import java.io.BufferedReader;
@@ -17,13 +17,12 @@ import java.util.Map;
 /**
  *
  * @author Thaís
-*/
-
+ */
 public class Armazem {
-    
-    protected static Map<String, Deus> deuses = new HashMap<>();
-    protected static Map<String, Habilidade> habilidades = new HashMap<>();
-    protected static Map<String, Jogador> jogadores = new HashMap<>();
+
+    private static Map<String, Deus> deuses = new HashMap<>();
+    private static Map<String, Habilidade> habilidades = new HashMap<>();
+    private static Map<String, Jogador> jogadores = new HashMap<>();
 
     //Escreve no arquivo deuses.txt
     public static void escreveDeuses() {
@@ -94,7 +93,6 @@ public class Armazem {
         try {
             //Checa se o arquivo existe e senão o cria
             if (!arquivo.exists()) {
-                System.out.println("Cria");
                 arquivo.createNewFile();
             }
             //construtor que recebe como primeiro argumento o objeto do tipo arquivo e como segundo se o conteúdo será acrescentado
@@ -116,35 +114,98 @@ public class Armazem {
         }
     }
 
+    //Escreve no arquivo Jogadores.txt
+    public static void escreveJogadores(Jogador j) {
+
+        //Instancia objeto do tipo File
+        File arquivo = new File("./src/main/java/com/mycompany/projetoonline/Jogadores.txt");
+
+        try {
+            //Checa se o arquivo existe e senão o cria
+            if (!arquivo.exists()) {
+                arquivo.createNewFile();
+            }
+            //construtor que recebe como primeiro argumento o objeto do tipo arquivo e como segundo se o conteúdo será acrescentado
+            //ao invés de ser substituído (append)
+            FileWriter fw = new FileWriter(arquivo, true);
+            //construtor recebe como argumento o objeto do tipo FileWriter
+            BufferedWriter bw = new BufferedWriter(fw);
+            //escreve no arquivoth
+            bw.write(j.getNome());
+            //quebra de linha
+            bw.newLine();
+            bw.write(j.getSenha());
+            bw.newLine();
+            //bw.write(j.getDeusEscolhido().getNome());
+            //bw.newLine();
+
+            if (j.isCadastrado()) {
+                bw.write("true");
+            } else {
+                bw.write("false");
+            }
+            bw.newLine();
+            if (j.isLogado()) {
+                bw.write("true");
+            } else {
+                bw.write("false");
+            }
+            bw.newLine();
+
+            //fecha os recursos
+            bw.close();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void atualizaJogadores(Map<String, Jogador> jogadores_) {
+        File arquivo = new File("./src/main/java/com/mycompany/projetoonline/Jogadores.txt");
+        try {
+            if (arquivo.exists()) {
+                arquivo.delete();
+                arquivo.createNewFile();
+            }
+            FileWriter fw = new FileWriter(arquivo, true);
+            //construtor recebe como argumento o objeto do tipo FileWriter
+            BufferedWriter bw = new BufferedWriter(fw);
+            //escreve no arquivo
+            for (String chave : jogadores.keySet()) {
+                Jogador jogador = jogadores.get(chave);
+                escreveJogadores(jogador);
+                leJogadores();
+            }
+            //fecha os recursos
+            bw.close();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void leDeuses() {
         File arquivo = new File("./src/main/java/com/mycompany/projetoonline/Deuses.txt");
         try {
-            if (arquivo.exists()) {
-                //construtor que recebe o objeto do tipo arquivo
+            if (!arquivo.exists()) {
+                arquivo.createNewFile();
+            } else {
                 FileReader fr = new FileReader(arquivo);
-                //construtor que recebe o objeto do tipo FileReader
                 BufferedReader br = new BufferedReader(fr);
 
-                //enquanto houver mais linhas
                 while (br.ready()) {
-                    //readLine() -> lê a proxima linha
                     String nome = br.readLine();
                     String descricao = br.readLine();
                     int vidaBase = Integer.parseInt(br.readLine());
                     int poderBase = Integer.parseInt(br.readLine());
                     int nivel = Integer.parseInt(br.readLine());
-                    //instancia objeto do tipo deus
                     Deus deus = new Deus(nome, descricao, vidaBase, poderBase, nivel);
-                    //armazena o deus no dicionario deuses, passando como chave o nome do deus
                     deuses.put(nome, deus);
-                    //imprime o objeto deus com chave nome
-                    System.out.println(deuses.get(nome));
                 }
-                //fecha os recursos
                 br.close();
                 fr.close();
-            } else {
-                System.out.println("Arquivo não encontrado!");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -154,7 +215,9 @@ public class Armazem {
     public static void leHabilidades() {
         File arquivo = new File("./src/main/java/com/mycompany/projetoonline/Habilidades.txt");
         try {
-            if (arquivo.exists()) {
+            if (!arquivo.exists()) {
+                arquivo.createNewFile();
+            } else {
                 FileReader fr = new FileReader(arquivo);
                 BufferedReader br = new BufferedReader(fr);
 
@@ -165,12 +228,9 @@ public class Armazem {
                     int carga = Integer.parseInt(br.readLine());
                     Habilidade habilidade = new Habilidade(nome, descricao, dano, carga);
                     habilidades.put(nome, habilidade);
-                    System.out.println(habilidades.get(nome));
                 }
                 br.close();
                 fr.close();
-            } else {
-                System.out.println("Arquivo não encontrado!");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -180,38 +240,41 @@ public class Armazem {
     public static void leJogadores() {
         File arquivo = new File("./src/main/java/com/mycompany/projetoonline/Jogadores.txt");
         try {
-            if (arquivo.exists()) {
+            if (!arquivo.exists()) {
+                arquivo.createNewFile();
+            } else {
                 FileReader fr = new FileReader(arquivo);
                 BufferedReader br = new BufferedReader(fr);
 
                 while (br.ready()) {
                     String nome = br.readLine();
                     String senha = br.readLine();
-                    String deus = br.readLine();
+                    //String deus = br.readLine();
                     boolean cadastrado = Boolean.parseBoolean(br.readLine());
                     boolean logado = Boolean.parseBoolean(br.readLine());
-                    Jogador jogador = new Jogador(nome, senha, deuses.get(deus), cadastrado, logado);
-                    //jogadores.put(nome, jogador);
-                    //System.out.println(jogador.get(nome));
+                    Jogador jogador = new Jogador(nome, senha, cadastrado, logado);
+                    jogadores.put(nome, jogador);
                 }
                 br.close();
                 fr.close();
-            } else {
-                System.out.println("Arquivo não existe!");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    
-    public static void main(String[] args) {
-        //leDeuses();
-        //leHabilidades();
-        //leJogadores();
-        //escreveJogadores();
-        escreveDeuses();
-    }
-    
-}
 
+    public static Map<String, Deus> getDeuses() {
+        leDeuses();
+        return deuses;
+    }
+
+    public static Map<String, Habilidade> getHabilidades() {
+        leHabilidades();
+        return habilidades;
+    }
+
+    public static Map<String, Jogador> getJogadores() {
+        leJogadores();
+        return jogadores;
+    }
+}
--- ProjetoOnline/src/main/java/com/mycompany/projetoonline/Jogador.java
+++ ProjetoOnline/src/main/java/com/mycompany/projetoonline/Jogador.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.projetoonline;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  *
  * @author mathe
@@ -60,7 +64,7 @@ public class Jogador {
 
     }
 
-    public Jogador(String nome, String senha,Deus deusEscolhido, boolean cadastrado, boolean logado) {
+    protected Jogador(String nome, String senha, boolean cadastrado, boolean logado) {
         this.deusEscolhido = deusEscolhido;
         this.nome = nome;
         this.senha = senha;
@@ -68,33 +72,144 @@ public class Jogador {
         this.logado = logado;
     }
 
-    
-    public Jogador() {
-        if (this.cadastrado == false) {
-            //Realize o Cadastro
+    //o aperto do botão cadastrar retorna false para cadastrado e 
+    //o do login retorna true, hipoteticamente
+    //System.out.println foi usado para testar a implementação
+    //a variavel deusEscolhido nao esta seno coniderada ainda
+    public Jogador(boolean cadastrado_) {
+        if (cadastrado_ == false) {
+            System.out.println("Cadastrar");
             this.cadastrar();
         } else {
-            //cadastrar();
-            // bota um bagui pra voltar pra tela inicial. ou só transforma o if em um while e dps continua.
-            //Realize o Login
-            String nomeDigitado = "0";
-            String senhaDigitada = "0";
-            if (this.verificarLogin(nomeDigitado, senhaDigitada)) {
-                this.logado = true;
-            }
+            System.out.println("Logar");
+            this.logar();
         }
     }
 
+    public void logar() {
+        //armazena os valores digitados pelo usuário
+        String nome_, senha_;
+        System.out.println("Digite seu nome: ");
+        Scanner teclado = new Scanner(System.in);
+        nome_ = teclado.nextLine();
+        System.out.println("Digite a senha: ");
+        senha_ = teclado.nextLine();
+
+        Map<String, Jogador> jogadores = Armazem.getJogadores();
+        
+        Jogador jogador = jogadores.get(nome_);
+
+        if (jogador == null) {
+            //usuario inexistente, faça o cadastro
+            System.out.println("Cadastre-se");
+            this.cadastrar();
+        } else {
+            this.logado = false;
+            while (this.logado == false) {
+                if (jogador.getSenha().equals(senha_)) {
+                    this.logado = true;
+                    this.nome = nome_;
+                    this.senha = senha_;
+                    this.cadastrado = true;
+                    break;
+                }
+                else{
+                //dados inválidos, tente novamente
+                //armazena os dados digitados pelo usuario
+                System.out.println("Digite seu nome: ");
+                nome_ = teclado.nextLine();
+                System.out.println("Digite a senha: ");
+                senha_ = teclado.nextLine();
+                jogador = jogadores.get(nome_);
+                }
+                
+     
+            }
+            jogadores.remove(nome_);
+            jogadores.put(this.nome,this);
+            Armazem.atualizaJogadores(jogadores);
+        }
+    }
+    
+
+    public String getSenha() {
+        return senha;
+    }
+
     private void deslogar() {
+        if(this.logado == true){
+        Map<String, Jogador> jogadores = Armazem.getJogadores();
+        
+        Jogador jogador = jogadores.get(this.nome);
+        jogadores.remove(this.nome);
         this.logado = false;
+            jogadores.put(this.nome,this);
+            Armazem.atualizaJogadores(jogadores);
+        }
     }
 
     private void cadastrar() {
+        //armazena os valores digitados pelo usuário
+        String nome_, senha_;
+        System.out.println("Digite seu nome: ");
+        Scanner teclado = new Scanner(System.in);
+        nome_ = teclado.nextLine();
+        System.out.println("Digite a senha: ");
+        senha_ = teclado.nextLine();
 
-        this.selecionarDeus();
+        Map<String, Jogador> jogadores = Armazem.getJogadores();
+        Jogador jogador = jogadores.get(nome_);
+
+        if (jogador == null) {
+            this.nome = nome_;
+            this.senha = senha_;
+            this.cadastrado = true;
+            selecionarDeus();
+        }
+        while (jogador != null) {
+            //armazena os novos valores digitados pelo usuário
+            System.out.println("Digite seu nome: ");
+            nome_ = teclado.nextLine();
+            System.out.println("Digite a senha: ");
+            senha_ = teclado.nextLine();
+
+            jogador = jogadores.get(nome_);
+
+            if (jogador == null) {
+                //não existe nenhum outro usuario com o mesmo nome
+                this.nome = nome_;
+                this.senha = senha_;
+                this.cadastrado = true;
+                this.selecionarDeus();
+                break;
+            }
+        }
+        if (cadastrado == true) {
+            Armazem.escreveJogadores(this);
+            jogadores = Armazem.getJogadores();
+        }
     }
 
-    private boolean verificarLogin(String nomeDigitado, String senhaDigitada) {
-        return false;
+    public boolean isCadastrado() {
+        return cadastrado;
     }
+
+    public boolean isLogado() {
+        return logado;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" + "nome=" + nome + ", senha=" + senha + ", cadastrado=" + cadastrado + ", logado=" + logado + '}';
+    }
+
+    public static void main(String[] args) {
+        Jogador j = new Jogador(true);
+        j.deslogar();
+    }
+    
 }
