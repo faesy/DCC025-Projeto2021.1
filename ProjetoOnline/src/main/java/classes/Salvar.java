e5139b3cc425a94e676725a696f2439c462c8f63
matheus <matheusfaesy@gmail.com>
2021-08-26 19:42:11 -0300

    começando função salvar

--- ProjetoOnline/src/main/java/classes/Salvar.java
+++ ProjetoOnline/src/main/java/classes/Salvar.java
@@ -32,7 +32,6 @@ public class Salvar implements ActionListener, BancoDados {
     JSONParser parser;
     Deus deus;
     private InsereRemoveObjetos objeto;
-    private String[] nomeHabilidades;
 
     public Salvar(Jogador jogador, InsereRemoveObjetos objeto) {
         this.objeto = objeto;
@@ -53,62 +52,27 @@ public class Salvar implements ActionListener, BancoDados {
         try {
             bancoDados = (JSONObject) parser.parse(new FileReader(CAMINHO_BANCO_DADOS));
             JSONArray bancoDadosArray = (JSONArray) bancoDados.get("Banco de Dados");
-            JSONObject deuses = (JSONObject) bancoDadosArray.get(1);
-            JSONArray deusesArray = (JSONArray) deuses.get("Deuses");
-            boolean existe = false;
-            for (int i = 0; i < deusesArray.size(); i++) {
-                JSONObject deusAux = (JSONObject) deusesArray.get(i);
-                if (deusAux.get("Nome").equals(deus.getNome())) {
-                    existe = true;
-                    JOptionPane.showMessageDialog(null, "O Deus já existe.", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-            if (existe == false) {
-                JSONObject deusAux = new JSONObject();
-                deusAux.put("Nome", deus.getNome());
-                deusAux.put("Descricao", deus.getDescricao());
-                deusAux.put("Poder Base", deus.getPoderBase());
-                deusAux.put("Vida Base", deus.getVidaBase());
-                deusAux.put("Nivel", deus.getNivel());
-                deusAux.put("Diretorio", deus.getCaminhoIcone());
-                deusAux.put("Tipo", deus.getTipo());
-                JSONObject habilidades = (JSONObject) bancoDadosArray.get(2);
-                JSONArray habilidadesArray = (JSONArray) habilidades.get("Habilidades");
-                JSONArray dHabilidadeArray = new JSONArray();
-                deusAux.put("Habilidades", dHabilidadeArray);
-                int[] hExiste = {0, 0, 0, 0};
-                for (int i = 0; i < habilidadesArray.size(); i++) {
-                    JSONObject habilidadeAux = (JSONObject) habilidadesArray.get(i);
-                    if (habilidadeAux.get("Nome").equals(nomeHabilidades[0])) {
-                        dHabilidadeArray.add(habilidadeAux);
-                        hExiste[0] = 1;
-                    }
-                    if (habilidadeAux.get("Nome").equals(nomeHabilidades[1])) {
-                        dHabilidadeArray.add(habilidadeAux);
-                        hExiste[1] = 1;
-                    }
-                    if (habilidadeAux.get("Nome").equals(nomeHabilidades[2])) {
-                        dHabilidadeArray.add(habilidadeAux);
-                        hExiste[2] = 1;
-                    }
-                    if (habilidadeAux.get("Nome").equals(nomeHabilidades[3])) {
-                        dHabilidadeArray.add(habilidadeAux);
-                        hExiste[3] = 1;
-                    }
-                }
-                for (int j = 0; j < hExiste.length; j++) {
-                    if (hExiste[j] == 0) {
-                        JOptionPane.showMessageDialog(null, "A habilidade " + nomeHabilidades[j] + " não existe. Crie-a primeiro ", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
-                        return;
-                    }
-                }
-                deusesArray.add(deusAux);
+            JSONObject deuses = (JSONObject) bancoDadosArray.get(0);
+            JSONArray jogadoresArray = (JSONArray) deuses.get("Jogadores");
+            for (int i = 0; i < jogadoresArray.size(); i++) {
+                JSONObject jogadorAux = (JSONObject) jogadoresArray.get(i);
+                if (jogadorAux.get("Nome").equals(deus.getNome())) {
+                    
+                jogadorAux.put("Chave de Progresso", jogador.getChaveProgresso());
+                
+                jogadorAux.put("Nivel", jogador.getDeus().getNivel());
+                    
+                jogadorAux.put("Deus Escolhido", jogador.getDeus().getNome());
+                
                 FileWriter fw = null;
                 fw = new FileWriter(CAMINHO_BANCO_DADOS);
                 fw.write(bancoDados.toJSONString());
                 fw.close();
-                JOptionPane.showMessageDialog(null, "Deus inserido com sucesso.", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
-            }
+                JOptionPane.showMessageDialog(null, "Jogo Salvo com sucesso", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+            
+                }
+            }      
+            
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Salvar.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
