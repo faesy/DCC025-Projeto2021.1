91393c307620d7c048da1c1704f8e464d3b022eb
Hychiro <hierohenrique@gmail.com>
2021-08-28 02:45:45 -0300

    toques finais

--- ProjetoOnline/src/main/java/classes/AlteraConsumiveis.java
+++ ProjetoOnline/src/main/java/classes/AlteraConsumiveis.java
@@ -38,66 +38,47 @@ public class AlteraConsumiveis {
         organizaArranjoConsumivel(nomeElixires);
 
         if (chave == 1) {
-
-            if (this.tipo.equals("Nordico")) {
-                for (Consumivel elixir : elixires) {
-                    if (elixir.getNome().contains("Poder Menor")) {
-                        jogador.setConsumivel(elixir);
-                        elixiresDoJogador = new Consumivel[1];
-                        elixiresDoJogador[0] = elixir;
-                        jogador.setConsumiveis(elixiresDoJogador);
-                    }
-                }
-            } else if (this.tipo.equals("Egipcio")) {
-                for (Consumivel elixir : elixires) {
-                    if (elixir.getNome().contains("Veneno Menor")) {
-                        jogador.setConsumivel(elixir);
-                        elixiresDoJogador = new Consumivel[1];
-                        elixiresDoJogador[0] = elixir;
-                        jogador.setConsumiveis(elixiresDoJogador);
-                    }
-                }
-            } else if (this.tipo.equals("Grego")) {
-                for (Consumivel elixir : elixires) {
-                    if (elixir.getNome().contains("Vida Menor")) {
-                        jogador.setConsumivel(elixir);
-                        elixiresDoJogador = new Consumivel[1];
-                        elixiresDoJogador[0] = elixir;
-                        jogador.setConsumiveis(elixiresDoJogador);
-                    }
-                }
-            }
+            elixiresDoJogador = new Consumivel[1];
+            tipoJogador(0);
 
         } else if (chave == 3) {
             criaArranjoConsumivel(2);
+            tipoJogador(2);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 6) {
             criaArranjoConsumivel(3);
+            tipoJogador(3);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 9) {
             criaArranjoConsumivel(4);
+            tipoJogador(4);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 12) {
             criaArranjoConsumivel(5);
+            tipoJogador(5);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 15) {
             criaArranjoConsumivel(6);
+            tipoJogador(6);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 18) {
             criaArranjoConsumivel(7);
+            tipoJogador(7);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 21) {
             criaArranjoConsumivel(8);
+            tipoJogador(8);
             jogador.setConsumiveis(elixiresDoJogador);
         } else if (chave == 24) {
             criaArranjoConsumivel(9);
+            tipoJogador(9);
             jogador.setConsumiveis(elixiresDoJogador);
         }
 
     }
 
     public void criaArranjoConsumivel(int tamanho) {
-        this.elixiresDoJogador = new Consumivel[tamanho];
+        this.elixiresDoJogador = new Consumivel[tamanho+1];
         for (int i = 0; i < tamanho; i++) {
             this.elixiresDoJogador[i] = this.elixires[i];
         }
@@ -111,6 +92,38 @@ public class AlteraConsumiveis {
             this.elixires[i] = g.pegaOConsumivelBancoDados(string);
             i++;
         }
+    }
+
+    public void tipoJogador(int i) {
+        if (this.tipo.equals("Nordico")) {
+            for (Consumivel elixir : elixires) {
+                if (elixir.getNome().contains("Poder Menor")) {
+                    jogador.setConsumivel(elixir);
+
+                    elixiresDoJogador[i] = elixir;
+                    jogador.setConsumiveis(elixiresDoJogador);
+                }
+            }
+        } else if (this.tipo.equals("Egipcio")) {
+            for (Consumivel elixir : elixires) {
+                if (elixir.getNome().contains("Veneno Menor")) {
+                    jogador.setConsumivel(elixir);
+
+                    elixiresDoJogador[i] = elixir;
+                    jogador.setConsumiveis(elixiresDoJogador);
+                }
+            }
+        } else if (this.tipo.equals("Grego")) {
+            for (Consumivel elixir : elixires) {
+                if (elixir.getNome().contains("Vida Menor")) {
+                    jogador.setConsumivel(elixir);
+
+                    elixiresDoJogador[i] = elixir;
+                    jogador.setConsumiveis(elixiresDoJogador);
+                }
+            }
+        }
+
     }
 //olha qual o deus do jogador (decide o tipo do elixir)(Cura=grego,veneno=egipcio,poder=nordico) 
     //olha qual a chave de progresso do jogador (decide a intensidade do elixir) 
